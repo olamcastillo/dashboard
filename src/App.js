26fdf89670de070f2336ebf0cf5055159c7fd56d
olamcastillo <Olamdiego@gmail.com>
2022-10-06 13:19:52 -0500

    context api

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import {  Nabbar, Footer, Sidebar, ThemeSettings, Navbar } from './components';
 import {  Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line } from './pages';
+import { useStateContext } from './contexts/ContexProvider';
 import './App.css';
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
   return (
     <div>
       <BrowserRouter>
