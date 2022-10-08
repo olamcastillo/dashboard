a7b3c2103cacb0c4a7f05a8d831ed01e6a9effe3
olamcastillo <Olamdiego@gmail.com>
2022-10-07 20:52:21 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-import {  Nabbar, Footer, Sidebar, ThemeSettings, Navbar } from './components';
+import {  Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import {  Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line } from './pages';
-import { useStateContext } from './contexts/ContexProvider';
 import './App.css';
+import { useStateContext } from './contexts/ContexProvider';
 
 const App = () => {
   const { activeMenu } = useStateContext();
@@ -25,13 +25,13 @@ const App = () => {
           </TooltipComponent>
         </div>
         {activeMenu ? (
-          <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
-            <Sidebar/>
-          </div>
+        <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+          <Sidebar/>
+        </div>
         ) : (
-          <div className='w-0 dark:bg-secondary-dark-bg'>
-            <Sidebar/>
-          </div>
+        <div className='w-0 dark:bg-secondary-dark-bg'>
+          <Sidebar/>
+        </div>
         )}
         <div className= { 
           `dark:bg-main-bg bg-main-bg min-h-screen w-full ${ activeMenu ? 'md:ml-72' : 'flex-2' }`
